5b202790d242b662c874695e33198d949ce75a13
kipngeno <kipngenokevin254@gmail.com>
2024-09-05 14:38:14 +0300

    updated Narok menu with the latest data

---
+++ js/hardcodedOffers.js
@@ -0,0 +1,78 @@
+export const hardCodedOffers = [
+    {
+        "offerName": "Sh5=70MB 1hr",
+        "uniqueOfferingId": "1222",
+        "offerValidity": 1,
+        "resourceAccId": 5158,
+        "resourceValue": 70,
+        "offerPrice": 5,
+        "offerUssdName": "Sh5=70MB 1hr",
+        "offeringId": 50502021,
+        "offerSource": "CVM1",
+        "locationId": null,
+        "subscribed": null,
+        "childOffers": []
+    },
+    {
+        "offerName": "Sh10=250MB 1hr",
+        "uniqueOfferingId": "1204",
+        "offerValidity": 1,
+        "resourceAccId": 5158,
+        "resourceValue": 250,
+        "offerPrice": 10,
+        "offerUssdName": "Sh10=250MB 1hr",
+        "offeringId": 50502021,
+        "offerSource": "CVM1",
+        "locationId": null,
+        "subscribed": null,
+        "childOffers": []
+    },
+    {
+        "offerName": "Sh10=100MB 24hr",
+        "uniqueOfferingId": "1057",
+        "offerValidity": 1,
+        "resourceAccId": 2572,
+        "resourceValue": 100,
+        "offerPrice": 10,
+        "offerUssdName": "Sh10=100MB 24hr",
+        "offeringId": 28042021,
+        "offerSource": "CVM2",
+        "locationId": null,
+        "subscribed": null,
+        "childOffers": [
+            {
+                "offerName": "Sh15=175MB for 24hr",
+                "offerValidity": 24,
+                "resourceAccId": 2572,
+                "resourceValue": 175,
+                "offerPrice": 15,
+                "offerUssdName": "For only sh5  more get 175MB",
+                "parentOfferId": 28042021
+            }
+        ]
+    },
+    {
+        "offerName": "Sh20=250MB 24hr",
+        "uniqueOfferingId": "1025",
+        "offerValidity": 1,
+        "resourceAccId": 2572,
+        "resourceValue": 250,
+        "offerPrice": 20,
+        "offerUssdName": "Sh20=250MB 24hr",
+        "offeringId": 28042021,
+        "offerSource": "CVM2",
+        "locationId": null,
+        "subscribed": null,
+        "childOffers": [
+            {
+                "offerName": "Sh25=300MB for 24hr",
+                "offerValidity": 24,
+                "resourceAccId": 2572,
+                "resourceValue": 300,
+                "offerPrice": 25,
+                "offerUssdName": "For only sh5  more get 300MB",
+                "parentOfferId": 28042021
+            }
+        ]
+    },
+];
